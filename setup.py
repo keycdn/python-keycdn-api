a9ee2df875face63d8f730af64c72106ed867e43
Sven <sven@keycdn.com>
2016-08-12 11:47:31 +0200

    Version update

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'keycdn',
   packages = ['keycdn'],
-  version = '0.2',
+  version = '0.3',
   description = 'A Python REST Client for KeyCDN API',
   author = 'KeyCDN',
   author_email = 'hello@keycdn.com',
   url = 'https://github.com/keycdn/python-keycdn-api',
-  download_url = 'https://github.com/keycdn/python-keycdn-api/tarball/0.2',
+  download_url = 'https://github.com/keycdn/python-keycdn-api/tarball/0.3',
   keywords = ['cdn', 'content delivery network', 'keycdn'],
   classifiers = [],
 )
