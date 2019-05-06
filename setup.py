4f685bb0e22a3d80ea2acdba60f573ca34aee7da
svenba <sven@proinity.com>
2019-05-06 18:19:16 +0200

    release 0.4

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
+import setuptools
 from distutils.core import setup
 setup(
   name = 'keycdn',
   packages = ['keycdn'],
-  version = '0.3',
+  version = '0.4',
   description = 'A Python REST Client for KeyCDN API',
   author = 'KeyCDN',
   author_email = 'hello@keycdn.com',
